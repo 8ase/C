b2275cd264e23b7abe626e868b42cd011cb3fc91
8ase <srajanjod@gmail.com>
2024-01-02 01:10:28 +0530

    Update src.c

--- C/src.c
+++ C/src.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
+    system("cls");
+    // printf("Hello World");
+    _Bool isHelloWorld = 1;
+    int rgbColor = 0xFFEF0D;
+    printf("%d", rgbColor);
     return 0;
 }
